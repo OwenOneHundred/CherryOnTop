821f934f695996e4cf4ce0b6af311eae97185733
AnthonyArminio <tonyarminio.0@gmail.com>
2025-02-17 23:22:26 -0500

    Switched to deltaTime-based attack rate system

--- Assets/Scripts/Items/Toppings/ToppingAttack.cs
+++ Assets/Scripts/Items/Toppings/ToppingAttack.cs
@@ -5,12 +5,12 @@ using UnityEngine;
  */
 public class ToppingAttack : MonoBehaviour
 {
-    // keeps track of the current targeted cherry. Should be null if there are no cherries in range
+    // Keeps track of the current targeted cherry. Should be null if there are no cherries in range
     GameObject targetedCherry;
-    // represents the number of frames to wait before attacking
-    int attackRate;
-    // keeps track of when the last attack was to time the next one
-    int timer;
+    // Represents the number of seconds to wait before attacking
+    float attackRate;
+    // Keeps track of when the last attack was to time the next one
+    float timer;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -24,12 +24,12 @@ public class ToppingAttack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(targetedCherry != null) {
-            if (timer == attackRate) {
+        if (targetedCherry != null) {
+            if (timer >= attackRate) {
                 attackCherry();
                 timer = 0;
             }
-            timer++;
+            timer += Time.deltaTime;
         } else {
             timer = 0;
         }

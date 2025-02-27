a1c3bbad58e4e5f36a0732153f91731b9a37a845
LeanneWong <leannewongschool@gmail.com>
2025-02-26 19:30:38 -0500

    Created a Projectile script to get damage from a projectile for Cherry Hitbox script

--- Assets/Scripts/Cherries/CherryHitbox.cs
+++ Assets/Scripts/Cherries/CherryHitbox.cs
@@ -5,15 +5,14 @@ using UnityEngine;
 /// </summary>
 public class CherryHitbox : MonoBehaviour
 {
-    public float cherryHealth;
-    public float damage = 5;
+    public float cherryHealth = 100;
     DebuffManager debuffManager;
 
     public void Start()
     {
         // Checks if cherry is big or not, then set cherryHealth.
         // May need to edit based on how big cherries are implemented.
-        if (gameObject.transform.localScale == new Vector3(2,2,2))
+        if (gameObject.transform.localScale == new Vector3(2, 2, 2))
         {
             cherryHealth = 75;
         }
@@ -21,24 +20,18 @@ public class CherryHitbox : MonoBehaviour
         {
             cherryHealth = 50;
         }
-
     }
 
     public void Update()
     {   
-        // Destroys cherry when health reaches 0
         if (cherryHealth == 0) {
             Destroy(gameObject);
         }
     }
 
-    private void OnTriggerEnter(Collider other)
+    public void TakeDamage(float damage)
     {
-        // Takes damage from projectile
-        if (other.GetComponent<Rigidbody>() != null)
-        {
-            cherryHealth -= debuffManager.GetDamageMultiplier() * damage;
-        }
-    }   
+        cherryHealth -= debuffManager.GetDamageMultiplier() * damage;
+    }
 
 }
---
+++ Assets/Scripts/Items/Toppings/Projectile.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+
+public class Projectile : MonoBehaviour
+{
+    public float damage;
+
+    public void Start()
+    {
+        SetDamage(damage);
+    }
+
+    public void SetDamage(float damage)
+    {
+        this.damage = damage;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Cherry"))
+        {
+            CherryHitbox cherry = other.gameObject.GetComponent<CherryHitbox>();
+            cherry.TakeDamage(damage);
+        }
+    }
+}

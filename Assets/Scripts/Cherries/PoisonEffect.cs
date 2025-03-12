f37b99a5ca53b0cf6f88a99b1f1a5b0109a2ee4f
LeanneWong <leannewongschool@gmail.com>
2025-03-12 17:46:13 -0400

    Added PoisonEffect script

---
+++ Assets/Scripts/Cherries/PoisonEffect.cs
@@ -0,0 +1,30 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.VFX;
+
+[CreateAssetMenu(menuName = "CherryDebuff/Slow")]
+public class PoisonEffect : CherryDebuff
+{
+    public override void EveryFrame()
+    {
+        // This is where effects would deal damage and operate logic.
+
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnAdded(GameObject cherry)
+    {
+        // Add VFX to cherry
+
+        // Set cherry field to the GameObject cherry argument
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnRemoved(GameObject cherry)
+    {
+        // Remove VFX from cherry
+
+        // Should not error when object is null
+        throw new System.NotImplementedException();
+    }
+}

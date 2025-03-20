6f3937459a25e4b9700d4687139fdfb6479e640c
AnthonyArminio <tonyarminio.0@gmail.com>
2025-03-19 20:04:17 -0400

    implemented LimitedAttack (untested)

--- Assets/Scripts/Items/Toppings/Attack System/ReactiveAttack.cs
+++ Assets/Scripts/Items/Toppings/Attack System/ReactiveAttack.cs
@@ -15,24 +15,24 @@ public class ReactiveAttack : ProjectileAttack
     [SerializeField]
     float firingDelay;
 
-    // Represents whether the Topping is free to attack (true) or is hindered by its cooldown (false)
-    private bool canAttack;
+    // Represents whether the Topping is free to attack (false) or is hindered by its cooldown (true)
+    private bool onCooldown = false;
 
     public override void OnStart() {
         Debug.Log("Reactive attack with a cooldown of " + this.cooldown + " seconds assigned to topping " + this.topping.name + ".");
     }
 
     public override void OnNewCherryFound(GameObject newTargetedCherry) {
-        if (canAttack) {
+        if (!onCooldown) {
             for (int i = 0; i < burstQuantity; i++) {
-                this.topping.GetComponent<AttackManager>().StartCoroutine(WaitAndAttack(newTargetedCherry, i * firingDelay));
+                this.topping.GetComponent<AttackManager>().StartCoroutine(DelayedAttack(newTargetedCherry, i * firingDelay));
             }
-            canAttack = false;
+            onCooldown = true;
         }
     }
 
     public override void OnCycle(GameObject targetedCherry) {
-        canAttack = true;
+        onCooldown = false;
     }
 
     public override void SpawnProjectile(GameObject projectile, Vector3 position, Vector3 velocity, Quaternion rotation, float damage) {
@@ -76,8 +76,8 @@ public class ReactiveAttack : ProjectileAttack
     /// <param name="targetedCherry"></param>
     /// <param name="waitTime"></param>
     /// <returns></returns>
-    private IEnumerator WaitAndAttack(GameObject targetedCherry, float waitTime) {
-        yield return new WaitForSeconds(waitTime);
+    private IEnumerator DelayedAttack(GameObject targetedCherry, float delay) {
+        yield return new WaitForSeconds(delay);
         AttackCherry(targetedCherry);
     }
 }

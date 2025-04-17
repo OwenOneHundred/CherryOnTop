93900471cf85b0999df4187ad333d35af34df093
aarondvc <adv69240@uga.edu>
2025-04-17 09:52:06 -0400

    Debug logging for buffer errors
    Added debug statements for the lollipop, some issue is occurring with the attack manager and possible race conditions.

--- Assets/Scripts/Items/Toppings/BuffZone.cs
+++ Assets/Scripts/Items/Toppings/BuffZone.cs
@@ -16,7 +16,7 @@ public class BuffZone : MonoBehaviour {
     // That means "coll => GetComponent<Collider>();" says "whenever you need the collider, run GetComponent." This is much slower than
     // running GetComponent once and storing the value to be read directly whenever it's needed. I've changed the code to that way.
 
-    void Start()
+    void Awake()
     {
         coll = GetComponent<Collider>();
         if (coll == null) {
@@ -24,6 +24,11 @@ public class BuffZone : MonoBehaviour {
         } else {
             coll.isTrigger = true; // Ensure it's a trigger.
         }
+
+         var rb = GetComponent<Rigidbody>();
+     if (rb == null) rb = gameObject.AddComponent<Rigidbody>();
+     rb.isKinematic = true;         // so physics won’t move it
+     rb.useGravity = false;  
         toppingLayer = LayerMask.GetMask("Topping");
     }
 
@@ -46,12 +51,35 @@ public class BuffZone : MonoBehaviour {
         HashSet<BuffManager> currentTowers = new HashSet<BuffManager>(); // holds everything found this frame
 
         foreach (Collider foundCollider in toppings) {
-            if (foundCollider.TryGetComponent(out BuffManager buffManager) && foundCollider != coll) {
+            Debug.Log($"Found collider: {foundCollider.gameObject.name}");
+            // using the line below for if check does work but probably costs more
+             BuffManager buffManager = foundCollider.GetComponentInParent<BuffManager>();
+            // foundCollider.TryGetComponent(out BuffManager buffManager);
+            if (buffManager != null) {
+                Debug.Log("found buff manager");
+            } else {
+                Debug.Log("found no buff manager");
+            }       
+
+    
+            if (foundCollider != coll) {
+                // This statement is not seen during tests
+                Debug.Log($"Found BuffManager: {buffManager.gameObject.name} and collider is not self");
 
                 currentTowers.Add(buffManager);
                 
-                if (!affectedToppings.Contains(buffManager) && foundCollider.TryGetComponent(out AttackManager _)){
-                    buffManager.AddBuff(this);
+                if (!affectedToppings.Contains(buffManager) && foundCollider.TryGetComponent(out AttackManager attackManager)) {
+                    // DID NOT PRINT BELOW
+                    Debug.Log("NEW RECORD");
+                    if(foundCollider.TryGetComponent(out ToppingObjectScript toppingObj)) {
+                        if ((toppingObj.topping.flags & flags) != 0) {
+                            // Check if the object's topping flags include the required flag.
+                            // Get its BuffManager.
+                            // Add the buff to the tower
+                            Debug.Log($"BuffZone {gameObject.name} applied {buffType} buff to {foundCollider.gameObject.name}");
+                            buffManager.AddBuff(this);
+                        }
+                    }
                 }
             }
         }
@@ -66,54 +94,80 @@ public class BuffZone : MonoBehaviour {
     }
 
 */
+
       // When an object enters the trigger zone:
-    void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other) {
+        Debug.Log("OnTriggerEnter called");
+
+    // 1) Layer check
+    if ((toppingLayer & (1 << other.gameObject.layer)) == 0)
+        return;
+    Debug.Log("Object is on topping layer");
+
+    // 2) Find the ToppingObjectScript on parent
+    var toppingObj = other.GetComponentInParent<ToppingObjectScript>();
+    if (toppingObj == null)
     {
-        // Check that the object is on the topping layer.
-        if ((toppingLayer & (1 << other.gameObject.layer)) != 0)
-        {
-            // First, ensure the object has the correct type via its ToppingObjectScript.
-            if (other.TryGetComponent(out ToppingObjectScript toppingObj))
-            {
-                // Check if the object's topping flags include the required flag.
-                if ((toppingObj.topping.flags & flags) != 0)
-                {
-                    // Get its BuffManager.
-                    if (other.TryGetComponent(out BuffManager buffManager))
-                    {
-                        if (!affectedToppings.Contains(buffManager))
-                        {
-                            buffManager.AddBuff(this);
-                            affectedToppings.Add(buffManager);
-                            Debug.Log($"BuffZone {gameObject.name} applied {buffType} buff to {other.gameObject.name}");
-                        }
-                    }
-                }
-            }
-        }
+        Debug.Log("  → no ToppingObjectScript found");
+        return;
+    }
+    Debug.Log("Object has ToppingObjectScript");
+
+    // 3) Flag filter
+    if ((toppingObj.topping.flags & flags) == 0)
+    {
+        Debug.Log("  → topping flags do not match");
+        return;
+    }
+    Debug.Log("Object has the required topping flags");
+
+    // 4) Find BuffManager on the same root
+    var buffManager = other.GetComponentInParent<BuffManager>();
+    if (buffManager == null)
+    {
+        Debug.Log("  → no BuffManager found");
+        return;
+    }
+    Debug.Log("Found BuffManager");
+
+
+    // 6) Finally apply buff if new
+    if (!affectedToppings.Contains(buffManager))
+    {
+        Debug.Log("Calling AddBuff()");
+        buffManager.AddBuff(this);
+        affectedToppings.Add(buffManager);
+    }
+    else
+    {
+        Debug.Log("  → buff already applied to this tower");
+    }
     }
 
     // When an object leaves the trigger zone:
     void OnTriggerExit(Collider other)
     {
+        Debug.Log("OnTriggerExit called");
         if ((toppingLayer & (1 << other.gameObject.layer)) != 0)
         {
             if (other.TryGetComponent(out BuffManager buffManager))
             {
                 if (affectedToppings.Contains(buffManager))
                 {
-                    buffManager.RemoveBuff(this);
+                    other.TryGetComponent(out AttackManager attackManager);
+                    buffManager.RemoveBuff(this,attackManager);
                     affectedToppings.Remove(buffManager);
                     Debug.Log($"BuffZone {gameObject.name} removed {buffType} buff from {other.gameObject.name}");
                 }
             }
         }
     }
+    
 
     void OnDestroy() {
         foreach (BuffManager buffManager in affectedToppings)
         {
-            buffManager.RemoveBuff(this);
+            buffManager.RemoveBuff(this, buffManager.GetComponentInParent<AttackManager>());
         }
     }
 

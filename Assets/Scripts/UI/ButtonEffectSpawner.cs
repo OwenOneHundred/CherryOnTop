b6291b7ffd40f926c1b72379de1505fd58154d13
Owen K <124847355+OwenOneHundred@users.noreply.github.com>
2025-06-27 23:00:32 -0400

    UI visual updates

--- Assets/Scripts/UI/ButtonEffectSpawner.cs
+++ Assets/Scripts/UI/ButtonEffectSpawner.cs
@@ -4,10 +4,13 @@ public class ButtonEffectSpawner : MonoBehaviour
 {
     [SerializeField] GameObject effectPrefab;
     [SerializeField] float lifetime = 4;
+    [SerializeField] float scaleMultiplier = 1;
     public void SpawnEffect()
     {
         Vector3 worldPoint = GetComponent<RectTransform>().position;
         worldPoint += Camera.main.transform.forward * 0.01f;
-        Destroy(Instantiate(effectPrefab, worldPoint, Quaternion.identity), lifetime);
+        GameObject newEffect = Instantiate(effectPrefab, worldPoint, Quaternion.identity, null);
+        Destroy(newEffect, lifetime);
+        newEffect.transform.localScale *= scaleMultiplier;
     }
 }
